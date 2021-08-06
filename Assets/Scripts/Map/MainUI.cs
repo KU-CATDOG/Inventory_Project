16377d92f45085e619608fbcd0ae047b3394403f
DESKTOP-UMN1IHM\user <vscalar02@gmail.com>
2021-08-06 18:18:55 +0900

    메민 화면 UI 적용

--- Assets/Scripts/Map/MainUI.cs
+++ Assets/Scripts/Map/MainUI.cs
@@ -6,12 +6,37 @@ using UnityEngine.SceneManagement;
 
 public class MainUI : MonoBehaviour
 {
+    public GameObject mainScreen;
+    public GameObject settingScreen;
+    public GameObject exitScreen;
+
+
     public void gameStartButton()
     {
         SceneManager.LoadScene("map0");
     }
-    public void Exit()
+    public void SettingButton()
+    {
+        settingScreen.SetActive(true);
+        mainScreen.SetActive(false);
+    }
+    public void SettingOutButton()
+    {
+        settingScreen.SetActive(false);
+        mainScreen.SetActive(true);
+    }
+    public void ExitQuestionButton()
+    {
+        exitScreen.SetActive(true);
+        mainScreen.SetActive(false);
+    }
+    public void ExitYesButton()
     {
         Application.Quit();
+    }
+    public void ExitNoButton()
+    {
+        exitScreen.SetActive(false);
+        mainScreen.SetActive(true);
     }
 }
