e6f32f09a82b6d03ea759349e7924f619232279b
Hyunjin <hyunjin1109@korea.ac.kr>
2021-08-10 14:18:35 +0900

    Applied Player Attack, Health; Applied Inventory Items

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -9,29 +9,94 @@ public class Player : MonoBehaviour
     public float maxHealth;
     public float attackDamage;
     public float attackSpeed;
-    public float moveSpeed = 5f;
+    public float moveSpeed;
     public bool isInvincible = false;
+
+    #region Stats
+    private float shieldDmg = 0f;
+    private float defense = 0f;
+    public float additionalMS = 0f;
+    public float additionalDMG = 0f;
+    private float tenacity = 0f;
+    #endregion
+
+    public Transform attackPoint;
+    public Transform hit;
+    public Vector3 tempAttackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
+    public Vector3 CursorPos;
+
+    public float attackRate = 2f;
+    float nextAttackTime = 0f;
 
     public Rigidbody2D rb;
     Vector2 movement;
 
+    public List<Items> Inventory;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        maxHealth = health = 10;
         attackDamage = 10f;
+        moveSpeed = 5f;
+        maxHealth = health = 10;
+        additionalDMG = attackDamage;
+        additionalMS = moveSpeed;
+
+        for (int i = 0; i < Inventory.Count; i++)
+        {
+            shieldDmg += Inventory[i].shieldDamage;
+            defense += Inventory[i].defense;
+            additionalMS += Inventory[i].moveSpeed;
+            additionalDMG += Inventory[i].damage;
+            tenacity += Inventory[i].tenacity;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        Vector3 cursorDir = mousePosition - attackPoint.position;
+        cursorDir = new Vector3(cursorDir.x, cursorDir.y, transform.position.z);
+
+        float cursorAngle = Mathf.Atan2(cursorDir.y, cursorDir.x) * Mathf.Rad2Deg;
+        bool isCursorRight = cursorAngle < 90 && cursorAngle > -90;
+        attackPoint.localScale = new Vector3(1, isCursorRight ? 1 : -1, 1);
+        attackPoint.rotation = Quaternion.Euler(0, 0, cursorAngle);
+
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        if(Time.time >= nextAttackTime)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Attack();
+                nextAttackTime = Time.time + 1f / attackRate;
+            }
+        }
+
     }
 
     private void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        rb.MovePosition(rb.position + movement * additionalMS * Time.fixedDeltaTime);
+    }
+
+    private void Attack()
+    {
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(hit.position, attackRange, enemyLayers);
+
+        foreach(Collider2D enemy in hitEnemies)
+        {
+            enemy.GetComponent<MonsterClass>().GetDamaged(additionalDMG);
+            Debug.Log("Enemy damage taken " + additionalDMG);
+        }
     }
 
     public void GetDamaged(float damage)
@@ -46,5 +111,12 @@ public class Player : MonoBehaviour
                 gameObject.SetActive(false);
             }
         }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (hit == null) return;
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(hit.position, attackRange);
     }
 }
