ee1493bb974b748467715b367f9e6695591ce35a
Rozkhara <nytemute@gmail.com>
2021-08-21 16:48:09 +0900

    fix Player

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -36,18 +36,12 @@ public class Player : MonoBehaviour
     public List<Items> Inventory;
 
     public testInvCon invCon;
+
     public GameObject InventoryManager;
     private int inventoryOpened;
-    //포션 맵 변수
-    Vector2 moveDir;
-    bool isSlippery = false;
-
-    //신발 맵 변수
-    public List<GameObject> SpeedUp, SpeedDown;
-    bool isOnSU = false;
-    bool isOnSD = false;
 
     float moveSoundTime = 0;
+
 
     // Start is called before the first frame update
     void Start()
@@ -67,18 +61,7 @@ public class Player : MonoBehaviour
             tenacity += Inventory[i].tenacity;
         }
 
-        if (FindObjectOfType<SpeedUp>() != null)
-        {
-            for (int i = 0; i < FindObjectsOfType<SpeedUp>().Length; i++)
-                SpeedUp.Add(FindObjectsOfType<SpeedUp>()[i].gameObject);
-        }
-        if (FindObjectOfType<SpeedDown>() != null)
-        {
-            for (int i = 0; i < FindObjectsOfType<SpeedDown>().Length; i++)
-                SpeedDown.Add(FindObjectsOfType<SpeedDown>()[i].gameObject);
-        }
     }
-
 
     // Update is called once per frame
     void Update()
@@ -106,6 +89,7 @@ public class Player : MonoBehaviour
             SoundManager.instance.PlayerWalk();
             moveSoundTime = Time.time + 0.3f;
         }
+
         if (Time.time >= nextAttackTime)
         {
             if (Input.GetMouseButtonDown(0))
@@ -114,25 +98,11 @@ public class Player : MonoBehaviour
                 nextAttackTime = Time.time + 1f / attackRate;
             }
         }
-        moveDir = new Vector2(movement.x, movement.y);
-        moveDir = moveDir.normalized;
-
-        if (isOnSU)
-            moveSpeed = 7f;
-        else if (isOnSD)
-            moveSpeed = 3f;
-        else
-            moveSpeed = 5f;
     }
 
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * additionalMS * Time.fixedDeltaTime);
-        if (isSlippery)
-            rb.AddForce(moveDir, ForceMode2D.Force);
-        else
-            rb.velocity = moveDir * moveSpeed;  //이동을 담당하는 코드가 바뀜
-                                                //rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 
     private void Attack()
@@ -162,25 +132,6 @@ public class Player : MonoBehaviour
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (SpeedUp.Contains(collision.gameObject))
-            isOnSU = true;
-        if (SpeedDown.Contains(collision.gameObject))
-            isOnSD = true;
-        if (collision.CompareTag("Slippery"))
-            isSlippery = true;
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (SpeedUp.Contains(collision.gameObject))
-            isOnSU = false;
-        if (SpeedDown.Contains(collision.gameObject))
-            isOnSD = false;
-        if (collision.CompareTag("Slippery"))
-            isSlippery = false;
-    }
     private void OnDrawGizmos()
     {
         if (hit == null) return;
@@ -212,5 +163,6 @@ public class Player : MonoBehaviour
             }
             InventoryManager.gameObject.SetActive(false);
         }
+
     }
 }
